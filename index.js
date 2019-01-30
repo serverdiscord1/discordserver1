d516254f8071c4153ec35c0b94a990d57fbc19a4
serverdiscord1 <47189330+serverdiscord1@users.noreply.github.com>
2019-01-30 20:01:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,27 +23,6 @@ client.on('message', message => {
 
 /////////////////////////////////////////////////////////////////////////////
 
-client.on("message", message => { //clear
-var args = message.content.substring(prefix.length).split(" ");
-if (message.content.startsWith(prefix + "clear")) {
-if(!message.channel.guild) return message.reply('**Sorry but this is for servers only ❌**');         
-if(!message.member.hasPermission('ADMINSTRATOR')) return message.reply('**You do not have permission to delete chat ⚠️**');
-  var msg;
-  msg = parseInt();
-
-message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-message.channel.sendMessage("", {embed: {
-  title: "``Chats cleared ✔️``",
-  color: 0x5016f3, 
-  footer: {
-    
-  }
-}}).then(msg => {msg.delete(3000)});
-}
-});
-
-/////////////////////////////////////////////////////////////////////////////
-
 client.on('message', message => {
 
   if (message.author.bot) return;
